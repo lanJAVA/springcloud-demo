286ad9d7fb47e8af6921a41f9cef89c2d192422b
'lan' <'13522554514@163.com'>
2017-08-02 14:15:21 +0800

    SpringCloud学习Demo--安全验证SpringSecurity
    1. 服务提供端加了SpringSecurity
    2. 服务消费端设置头信息访问

--- SpringCloudDemo-consumer-80/src/main/java/org/SpringCloudDemo/controller/ConsumerDeptController.java
+++ SpringCloudDemo-consumer-80/src/main/java/org/SpringCloudDemo/controller/ConsumerDeptController.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 import org.SpringCloudDemo.vo.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -25,23 +28,31 @@ public class ConsumerDeptController {
   
   @Autowired
   private RestTemplate restTemplate;
+  @Autowired
+  private HttpHeaders headers;
   
   @RequestMapping("/consumer/dept/get")
   public Object getDept(long id) {
-    Dept dept = this.restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+//    Dept dept = this.restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+    Dept dept = this.restTemplate.exchange(DEPT_GET_URL + id, HttpMethod.GET, 
+         new HttpEntity<Object>(this.headers), Dept.class).getBody();
     return dept;
   }
   
   @SuppressWarnings("unchecked")
   @RequestMapping("/consumer/dept/list")
   public Object listDept() {
-    List<Dept> allDepts = this.restTemplate.getForObject(DEPT_LIST_URL, List.class);
+//    List<Dept> allDepts = this.restTemplate.getForObject(DEPT_LIST_URL, List.class);
+    List<Dept> allDepts = this.restTemplate.exchange(DEPT_LIST_URL, HttpMethod.GET,
+        new HttpEntity<Object>(this.headers), List.class).getBody();
     return allDepts;
   }
   
   @RequestMapping("/consumer/dept/add")
   public Object addDept(Dept dept) {
-    Boolean flag = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+//    Boolean flag = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+    Boolean flag = this.restTemplate.exchange(DEPT_ADD_URL, HttpMethod.POST,
+        new HttpEntity<Object>(dept, this.headers), Boolean.class).getBody();
     return flag;
   }
   
