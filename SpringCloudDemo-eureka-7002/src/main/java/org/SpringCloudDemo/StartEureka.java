c94ad1449c72e74f9e810e2dfadd07f24adab09a
'lan' <'13522554514@163.com'>
2017-08-03 11:25:29 +0800

    SpringCloud学习Demo--Eureka
    1. ha高可用机制

---
+++ SpringCloudDemo-eureka-7002/src/main/java/org/SpringCloudDemo/StartEureka.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2017 - 数多科技
+ * 
+ * 北京数多信息科技有限公司
+ * 本公司保留所有下述内容的权利。
+ * 本内容为保密信息，仅限本公司内部使用。
+ * 非经本公司书面许可，任何人不得外泄或用于其他目的。
+ */
+package org.SpringCloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class StartEureka {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StartEureka.class, args);
+  }
+
+}
